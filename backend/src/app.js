43015c2dfc18d533876792c36857ec66996870c4
Devendra Sonawane <dpsonawane789@gmail.com>
2025-12-25 17:44:05 +0530

    setup user model for creating user document in MongoDB with token generation using bcryptjs

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 
@@ -8,6 +9,9 @@ app.use(cors({
     origin: 'https://localhost:5173',
     credentials: true
 }));
+
+// --- Routes ---
+app.use('/api/users', userRoutes);
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
