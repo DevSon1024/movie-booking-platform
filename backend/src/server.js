bdef8d3075c5973316601aa2d1e505acd8b41119
Devendra Sonawane <dpsonawane789@gmail.com>
2025-12-25 11:24:58 +0530

    First Commit

---
+++ backend/src/server.js
@@ -0,0 +1,8 @@
+// server/server.js
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => res.send('API is running...'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
